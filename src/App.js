06074321c1d8f247e8922551fd1098c719bebb4a
jinxx45 <www.pragat7@pec.edu>
2021-07-21 22:16:35 +0530

    landing page update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <div className="image-bg">
+        <div className="blur">
+
+        <div className="main-content d-flex  justify-content-center ">
+          <div className="main-card d-flex align-items-center flex-column justify-content-center">
+            <h2 className="fw-bold display-1">React Birthday Reminder</h2>
+            <button style={{width:"120px"}} className="justify-content-center btn btn-outline-danger btn-lg fw-bold mx-5"> Launch</button>
+          </div>
+          
+        </div>
+        </div>
+        
+      </div>
+   
+      
+
     </div>
   );
 }
