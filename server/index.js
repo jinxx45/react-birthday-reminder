eb729270c11f33e8246cc661bf6f1cfd0317aa09
jinxx45 <www.pragat7@pec.edu>
2021-07-25 23:32:38 +0530

    server side update

--- server/index.js
+++ server/index.js
@@ -5,11 +5,11 @@ const app = express();
 app.use(express.json({limit: "30mb",extended:true}));
 app.use(express.urlencoded({limit: "30mb",extended:true}));
 app.use(cors())
-import itemRoutes from './routes/items.js';
+import itemRoutes from './routes/item.js';
 app.use('/items',itemRoutes)
 app.get('/',(req,res)=>{
 res.send('Hello')
 })
-const mongodb = "mongodb+srv://ckmobile:password@nodetuts.qwlpv.mongodb.net/items-database?retryWrites=true&w=majority";
+const mongodb = "mongodb+srv://jinx:<password>@birthdays.eexug.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 mongoose.connect(mongodb,{ useNewUrlParser: true, useUnifiedTopology: true }).then(()=>app.listen(PORT,console.log(`Server running on ${PORT}`))).catch(err=>console.log(err));
